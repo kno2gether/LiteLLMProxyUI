ded618bdb1289f973535acce0a386223b2c95d4a
Avijit sarkar <avijeett007@gmail.com>
2023-12-19 16:05:42 +0000

    System generated commit, initial commit

---
+++ pages/Home/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,12 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
